e9199a0871a1b184e7d6743d1e6cc6ecd3ced53b
Elias Dräxler <elias@draexler.at>
2014-10-19 20:11:36 +0200

    comments

--- src/pb.c
+++ src/pb.c
@@ -17,15 +17,15 @@ int main(int argc, char** argv)
         fprintf(stderr, "Usage: %s power_ball (5 white balls)\n", argv[0]);
         return -1;
     }
-
+	//powerball
     int power_ball = atoi(argv[1]);
-
+	//white ball
     int white_balls[5];
     for (int i=0; i<5; i++)
     {
         white_balls[i] = atoi(argv[2+i]);
     }
-
+	//result
     int result = calculate_result(white_balls, power_ball);
 
     printf("%d percent chance of winning\n", result);
